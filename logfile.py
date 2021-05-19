de0a14ddc62adc5a7fc235ade0944b351a6396a1
David Poole <dpoole@cradlepoint.com>
2021-05-19 16:29:45 -0600

    fix issues ; add debugging
    - make copy of incoming port_numbers array because we modify it w/i the
      function
    - add some more print statements
    - fix main() to use new fn name

--- logfile.py
+++ logfile.py
@@ -35,6 +35,9 @@ def parse_logfile_portstatus(infilename, port_numbers):
     print(line_list)
     print(port_numbers)
 
+    # this function modifies this list so make a copy of port_numbers
+    port_numbers = list(port_numbers)    
+
     timestamps = {}
     # start at the bottom of list, continue upwards until all port numbers have been found
     line_list.reverse()
@@ -45,12 +48,16 @@ def parse_logfile_portstatus(infilename, port_numbers):
         # capture the path to find the port number
         if "deltatime" in line:
             ts = int(line[line.index("=")+1:])
+            print(f"ts={ts}")
         elif "path=" in line:
             port = re_num.search(line)
+            if not port:
+                raise ValueError("Missing port number in line=%r" % line)
             port = int(port.group(1))
             if ts == 0:
                 raise ValueError(infilename + " format error")
             
+            print(f"port={port} port_numbers={port_numbers}")
             if port != port_numbers[-1]:
                 raise ValueError("out of sequence: %d != %d" % (port, port_numbers[-1]))
             port_numbers.pop()
@@ -68,9 +75,8 @@ def main():
     # testing
     port_counters = {0: None, 1: None, 2: None, 3: None, 4: None, 5: None, 6: None, 7: None, 8: None, 9: None}
     port_numbers = list(port_counters.keys())
-    timestamps = parse_logfile("/tmp/minicom.cap", port_numbers)
+    timestamps = parse_logfile_portstatus("/tmp/minicom.cap", port_numbers)
     print(f"timestamps={timestamps}")
-    for p in port_counters:
 
 if __name__ == '__main__':
     main()

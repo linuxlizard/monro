f944e1fd9498a7573f94c596cb216e4b68636387
David Poole <dpoole@cradlepoint.com>
2021-05-19 16:31:23 -0600

    it works!
    - finish the /status/ethernet stats integration
    - remove logfile (racy and won't work on routers not serial attached)
    - read from a new firmware specific /status/event/ethernet which holds
      the timestamp (see dpoole/measure-status-ethernet)

--- serv.py
+++ serv.py
@@ -1,36 +1,66 @@
-import numpy as np
+# Simple tornado/bootstrap web app to read/watch/poll/study a Cradlepoint router.
+#
+# David Poole 
+# davep@mbuf.com 20210519
 
+import time
+
+import numpy as np
 import tornado.ioloop
 import tornado.web
 
 from cpnetem import webapi
-import minicom
+import logfile
 
 hostname = "172.16.253.1"
 
 class EthernetPortStatistics:
-    def __init__(self):
+    # capture the deltatime in reading the port status
+    def __init__(self, port_number):
+        self.port = port_number
         self.last = 0
         # note will grow without bound but this little program is only intended
         # to run for a short time anyway
         self.samples = []
 
     def update(self, n):
+        self.last = n
         self.samples.append(n)
+        print(f"port={self.port} last={self.last} samples={self.samples}")
 
     def stats(self):
         ndata = np.asarray(self.samples)
-        return { "min": ndata.min(),
+        return { "last": self.last,
+                "min": ndata.min(),
                 "max" : ndata.max(),
                 "mean" : ndata.mean(),
                 "median" : np.median(ndata),
                 "std" : ndata.std() }
 
+class EthernetDeviceMeasure:
+    def __init__(self, hostname, ports_number_list):
+        self.hostname = hostname
+        self.ports = ports_number_list
+        self.port_stats = { k:EthernetPortStatistics(k) for k in ports_number_list }
+        print(f"ports_number_list={ports_number_list} self.port_stats={self.port_stats}")
+
+    def update(self, port_stats):
+        # port_stats is a dict
+        #   key: port number
+        #   value: new deltatime sample
+        print(f"update hostname={self.hostname} ports={self.ports} new={port_stats}")
+        for p in port_stats:
+            print(f"update p={p}")
+            self.port_stats[p['port']].update(p['deltatime'])
+
+    @property
+    def stats(self):
+        return { k:self.port_stats[k].stats() for k in self.ports }
+
 # dict of of /status/ethernet sampling
 # key: hostanme
-# value: dict of
-#   key: port number
-#   value: EthernetPortStatistics
+# value: EthernetDeviceMeasure
+# TODO make async/thread safe
 ethernet_stats = {}
 
 class MainHandler(tornado.web.RequestHandler):
@@ -55,11 +85,27 @@ class ProductInfoHandler(RouterHandler):
 class StatusEthernetHandler(RouterHandler):
     def get(self):
         conn = self.get_conn()
+
+        # who are we talking to?
+        product_info = conn.get("/api/status/product_info")
+        print(f"product={product_info}")
+
+        # get the port status
         status = conn.get("/api/status/ethernet")
         print(f"status={status}")
-        port_numbers = { d["port"] : None for d in status }
+        port_numbers = [ d["port"] for d in status ]
         print(f"port_numbers={port_numbers}")
-        self.render("ethernet.html", ports=status )
+
+        # now get the timestamps
+        deltatimes = conn.get("/api/status/event/ethernet")
+        
+        print(f"deltatimes={deltatimes}")
+        if hostname not in ethernet_stats:
+            ethernet_stats[hostname] = EthernetDeviceMeasure(hostname, port_numbers)
+        ethernet_stats[hostname].update(deltatimes)
+
+        print(f"stats={ethernet_stats[hostname].stats}")
+        self.render("ethernet.html", product_info=product_info, ports=status, stats=ethernet_stats[hostname].stats )
 
 def make_app():
     return tornado.web.Application([

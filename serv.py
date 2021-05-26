b53ff00379552ea9b472d7260d08e00ab8358d43
David Poole <dpoole@cradlepoint.com>
2021-05-26 08:20:42 -0600

    add LAN status thread
    - read /status/lan/stats in a thread storing the values every 5 seconds
    - add an api endpoint to get /status/lan/stats history as CSV
    - add static file handling to serve js

--- serv.py
+++ serv.py
@@ -3,11 +3,15 @@
 # David Poole 
 # davep@mbuf.com 20210519
 
+import os
 import time
+import threading
+import collections
 
 import numpy as np
 import tornado.ioloop
 import tornado.web
+import tornado.httpclient
 
 from cpnetem import webapi
 import logfile
@@ -19,8 +23,8 @@ class EthernetPortStatistics:
     def __init__(self, port_number):
         self.port = port_number
         self.last = 0
-        # note will grow without bound but this little program is only intended
-        # to run for a short time anyway
+        # XXX note will grow without bound but this little program is only
+        # intended to run for a short time anyway
         self.samples = []
 
     def update(self, n):
@@ -57,17 +61,97 @@ class EthernetDeviceMeasure:
     def stats(self):
         return { k:self.port_stats[k].stats() for k in self.ports }
 
-# dict of of /status/ethernet sampling
-# key: hostanme
+
+class LANStatusThread(threading.Thread):
+    def __init__(self, stats_dict, maxlen=256):
+        super().__init__(name="LANStatus")
+
+        self.stats = collections.deque(maxlen=maxlen)
+
+        self.statslock = threading.Lock()
+        self.quit = threading.Event()
+
+        # stats_dict is a dict from /status/lan/stats
+        # save initial sample
+        self.update(stats_dict)
+
+        # build the CSV header safe (safe to do it now because the names don't
+        # change @ run-time)
+        self.csv_header_str = ",".join(self.stats[0].keys()) + "\n"
+
+    def run(self):
+        while not self.quit.is_set():
+            # TODO obviously need better way to get the URL
+            url = "172.16.22.1:80" 
+            http = tornado.httpclient.HTTPClient()
+            response = http.fetch(f"http://{url}/api/status/lan/stats", 
+                                        auth_username="admin", 
+                                        auth_password=os.getenv("CP_PASSWORD"))
+            json = tornado.escape.json_decode(response.body)
+            stats = json['data']
+            print(f"lanstats stats={stats}")
+            self.update(stats)
+            time.sleep(5)
+
+    def stop(self):
+        # WARNING! will be called outside thread context
+        self.quit.set()
+        self.wait()
+
+    def get_csv(self):
+        # WARNING will be called outside thread context
+
+        # build an array of arrays of stats
+        csv_str = self.csv_header_str
+
+        with self.statslock:
+            stats_iter = iter(self.stats)
+            for stats_dict in stats_iter:
+                csv_str += ",".join([str(n) for n in stats_dict.values()]) + "\n"
+
+        return csv_str
+
+    def update(self, stats_dict):
+        ts = time.time()
+        d = dict(stats_dict)
+        d["timestamp"] = time.time()
+
+        with self.statslock:
+            self.stats.append(d)
+
+
+# dict of /status/ethernet sampling
+# key: hostname
 # value: EthernetDeviceMeasure
 # TODO make async/thread safe
 ethernet_stats = {}
+
+# thread poll on /status/lan/stats
+lan_stats_thread = None
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
+class ClientHandler(tornado.web.RequestHandler):
+    async def get(self):
+        print("ClientHandler get")
+        http = tornado.httpclient.AsyncHTTPClient()
+        response = await http.fetch("http://172.16.253.1/api/status/wlan", 
+                                    auth_username="admin", 
+                                    auth_password=os.getenv("CP_PASSWORD"))
+        json = tornado.escape.json_decode(response.body)
+        print(f"json={json}")
+#        self.write("Fetched " + str(len(json["entries"])) + " entries "
+#                   "from the FriendFeed API")
+        self.write("Hello, world")
+        
 class RouterHandler(tornado.web.RequestHandler):
+    def get_args(self):
+        router = self.get_argument("router", default=hostname)
+        port = self.get_argument("port", default="80")
+        return "%s:%s" % (router, port)
+
     def get_conn(self):
         router = self.get_argument("router", default=hostname)
         port = self.get_argument("port", default="80")
@@ -75,12 +159,37 @@ class RouterHandler(tornado.web.RequestHandler):
         return conn
 
 class ProductInfoHandler(RouterHandler):
-    def get(self):
-        conn = self.get_conn()
-        product_info = conn.get("/api/status/product_info")
-        print(product_info)
+    async def get(self):
+#        conn = self.get_conn()
+#        product_info = conn.get("/api/status/product_info")
+#        print(product_info)
+        url = self.get_args()
+        http = tornado.httpclient.AsyncHTTPClient()
+        response = await http.fetch(f"http://{url}/api/status/product_info", 
+                                    auth_username="admin", 
+                                    auth_password=os.getenv("CP_PASSWORD"))
+        json = tornado.escape.json_decode(response.body)
+        print(f"json={json}")
+        product_info = json['data']
         self.render("product_info.html", 
             product_info = product_info )
+
+class APILanHandler(RouterHandler):
+    def get(self):
+        self.set_header("Content-Type", "text/csv")
+        self.write(lan_stats_thread.get_csv())
+
+class LanHandler(RouterHandler):
+    async def get(self):
+        url = self.get_args()
+        http = tornado.httpclient.AsyncHTTPClient()
+        response = await http.fetch(f"http://{url}/api/status/lan/stats", 
+                                    auth_username="admin", 
+                                    auth_password=os.getenv("CP_PASSWORD"))
+        json = tornado.escape.json_decode(response.body)
+        stats = json['data']
+        print(f"stats={stats}")
+        self.render("lan.html", stats=stats)
 
 class StatusEthernetHandler(RouterHandler):
     def get(self):
@@ -112,9 +221,34 @@ def make_app():
         (r"/", MainHandler),
         (r"/product_info", ProductInfoHandler),
         (r"/ethernet", StatusEthernetHandler),
-    ])
+        (r"/client", ClientHandler),
+        (r"/lan", LanHandler),
+        (r"/api/lan", APILanHandler),
+#        (r"/lan.js", tornado.web.StaticFileHandler, {"path":"."} ),
+        ],
+        debug=True,
+        static_path="./js",
+    )
+
+def init_lan_status_thread():
+    global lan_stats_thread
+    url = "172.16.22.1:80" 
+    http = tornado.httpclient.HTTPClient()
+    response = http.fetch(f"http://{url}/api/status/lan/stats", 
+                                auth_username="admin", 
+                                auth_password=os.getenv("CP_PASSWORD"))
+    json = tornado.escape.json_decode(response.body)
+    stats = json['data']
+    print(f"lanstats stats={stats}")
+    lan_stats_thread = LANStatusThread(stats)
+    lan_stats_thread.start()
 
 if __name__ == "__main__":
     app = make_app()
     app.listen(8888)
+
+    init_lan_status_thread()
+
     tornado.ioloop.IOLoop.current().start()
+
+# vim: ts=4:sts=4:et

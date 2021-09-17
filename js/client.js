5bc8ae74cefb046cd9f439cccda569c24dd7e1e6
David Poole <dpoole@cradlepoint.com>
2021-09-17 17:12:50 -0600

    start of polling for new analytics data

--- js/client.js
+++ js/client.js
@@ -1,3 +1,32 @@
+'use strict';
+
+function poll_client(url, macaddr) 
+{
+//	d3.json
+}
+
+function csv_path()
+{
+	const target = new URL(document.URL);
+	return target.origin + "/api/wlan/analytics/client" + target.search;
+}
+
+function json_path()
+{
+	const target = new URL(document.URL);
+	return target.origin + "/api/status/wlan/analytics?router=" + target.searchParams.get('router');
+}
+
+function find_client(r_json, macaddr)
+{
+	// find client 'macaddr' in /status/wlan/analytics
+	console.log(`find_client macaddr=${macaddr}`);
+
+	r_json.clients.forEach(function(value,idx,arr) {
+		console.log(`found client=${value.macaddr}`)
+	});
+}
+
 window.onload=function() 
 {
 	console.log("hello client.js");
@@ -6,12 +35,10 @@ window.onload=function()
 	const width = d3.select("#tx_packets.graph").node().clientWidth - 50;
 	console.log(`width=${width}`);
 
-	let tx_packets_graph = new Graph(width, height,"tx_packets","#tx_packets");
+	let tx_packets_graph = new GraphDate(width, height,"tx_packets","#tx_packets");
+	let rx_packets_graph = new GraphDate(width, height,"rx_packets","#rx_packets");
 
-	const target = new URL(document.URL);
-	const path = target.origin + "/api/wlan/analytics/client" + target.search;
-
-	d3.csv(path)
+	d3.csv(csv_path())
 	.then(function(data) {
 		console.table(data);
 
@@ -22,8 +49,26 @@ window.onload=function()
 		});
 
 		tx_packets_graph.draw(data);
+		rx_packets_graph.draw(data);
 
 	});
 
+	let poll_analytics = (function(poll_url) {
+		const url = poll_url;
+		return function() { return poll_client(url) };
+	});
+
+	console.log(`json_path=${json_path()}`);
+	d3.json(json_path()).
+		then( function(j_analytics) {
+//			console.table(j_analytics.data.clients);
+			const target = new URL(document.URL);
+			const macaddr = target.searchParams.get("macaddr");
+			find_client(j_analytics['data'], macaddr);
+		}
+	);
+
+//	poll_analytics()();
+//	setInterval( function() { 
 }
 
